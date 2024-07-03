3e1e87af2fa4372e4abd46311b75bf28b8004159
dundic <tadaleck@gmail.com>
2024-07-03 14:28:38 +0300

    next commit

--- camelCase.js
+++ camelCase.js
@@ -1,4 +1,4 @@
-//Assign the string "Test" to your secondCharacter variable below your declaration. Open the console to see how your log has changed.
+//Assign the string "Test" to your secondCharacter variable below your declaration. Open the console to see how your log has changed.gi
 
 let secondCharacter;
 secondCharacter = "Test";
