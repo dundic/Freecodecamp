02c53ac1bff91b0bd3feab194c77ec0be90e684f
dundic <tadaleck@gmail.com>
2024-07-04 13:14:55 +0300

    next commit

---
+++ fillOfArray.js
@@ -0,0 +1,2 @@
+//Change your rows declaration to be an array with the strings "Naomi", "Quincy", and "CamperChan". The order of values in an array is important, so follow that order. Remember that strings are case-sensitive.
+let rows = ['Naomi', 'Quincy', 'CamperChan'];
